d52f7811a239f24f0fd27733b1719d22b3951c5b
David Rubinger <david.rubinger@gmail.com>
2019-08-05 15:26:07 -0400

    Add Docstrings

--- create_tables.py
+++ create_tables.py
@@ -4,12 +4,16 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """Remove all tables."""
+
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """Create all tables."""
+    
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
--- etl.py
+++ etl.py
@@ -4,12 +4,16 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """Copy data from S3 to staging tables on Redshift."""
+
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """Insert data from Redshift staging tables to analytics tables on Redshift."""
+
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
